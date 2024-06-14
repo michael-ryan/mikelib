f372555cc906decdbfba2d6fef63eddac6fce045
Michael Ryan <ryan.m1334@gmail.com>
2024-06-14 23:08:20 +0100

    Refactor test to be idiomatic go style test

--- streams/streams_test.go
+++ streams/streams_test.go
@@ -5,19 +5,32 @@ import (
 )
 
 func TestMapDoubles(t *testing.T) {
-	xs := []int{1, 2, 3, 4, 5}
-	doubleFunc := func(x int) int { return 2 * x }
-
-	ys := Map(xs, doubleFunc)
-
-	expected := []int{2, 4, 6, 8, 10}
-	if len(ys) != len(expected) {
-		t.Errorf("len(ys) = %v, got %v", len(ys), len(expected))
+	var tests = []struct {
+		xs   []int
+		want []int
+		name string
+	}{
+		{
+			xs:   []int{1, 2, 3, 4, 5},
+			want: []int{2, 4, 6, 8, 10},
+			name: "1..5",
+		},
 	}
 
-	for i := range len(ys) {
-		if ys[i] != expected[i] {
-			t.Errorf("ys[%v] = %v, got %v", i, ys[i], expected[i])
-		}
+	doubleFunc := func(x int) int { return 2 * x }
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ys := Map(tt.xs, doubleFunc)
+			if len(ys) != len(tt.want) {
+				t.Errorf("len(ys) = %v, got %v", len(ys), len(tt.want))
+			}
+
+			for i := range len(ys) {
+				if ys[i] != tt.want[i] {
+					t.Errorf("ys[%v] = %v, got %v", i, ys[i], tt.want[i])
+				}
+			}
+		})
 	}
 }
