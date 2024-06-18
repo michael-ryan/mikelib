22af8ec12354a1c90c99d58d43d07c1fcd1b59aa
Michael Ryan <ryan.m1334@gmail.com>
2024-06-18 14:13:54 +0100

    Document updates, migrate to public struct, ditch the generics

--- pkg/vec/vec2.go
+++ pkg/vec/vec2.go
@@ -5,28 +5,23 @@ import (
 	"math"
 )
 
-// vec2 represents a vector in 2D space.
+// Vec2 represents a vector in 2D space.
 // Many receivers are provided - note these are value receivers,
-// and therefore never modify the vec2 being operated upon.
-//
-// vec2s may hold any float or integer type, but note that you may only operate on like-for-like.
-// That is to say, for example, you may only multiply a vec2[float64] with another vec2[float64].
-//
-// It is recommended that you choose one underlying numeric type, and stick to it.
-type vec2[T number] struct {
-	X, Y T
+// and therefore never modify the Vec2 being operated upon.
+type Vec2 struct {
+	X, Y float64
 }
 
 // NewVec2 returns a new 2D vector.
-func NewVec2[T number](x T, y T) vec2[T] {
-	return vec2[T]{
+func NewVec2(x, y float64) Vec2 {
+	return Vec2{
 		X: x,
 		Y: y,
 	}
 }
 
 // Add computes v1 + v2.
-func (v1 vec2[T]) Add(v2 vec2[T]) vec2[T] {
+func (v1 Vec2) Add(v2 Vec2) Vec2 {
 	return NewVec2(
 		v1.X+v2.X,
 		v1.Y+v2.Y,
@@ -34,7 +29,7 @@ func (v1 vec2[T]) Add(v2 vec2[T]) vec2[T] {
 }
 
 // Subtract computes v1 - v2.
-func (v1 vec2[T]) Subtract(v2 vec2[T]) vec2[T] {
+func (v1 Vec2) Subtract(v2 Vec2) Vec2 {
 	return NewVec2(
 		v1.X-v2.X,
 		v1.Y-v2.Y,
@@ -42,12 +37,12 @@ func (v1 vec2[T]) Subtract(v2 vec2[T]) vec2[T] {
 }
 
 // Dot computes the dot product between v1 and v2.
-func (v1 vec2[T]) Dot(v2 vec2[T]) T {
+func (v1 Vec2) Dot(v2 Vec2) float64 {
 	return v1.X*v2.X + v1.Y*v2.Y
 }
 
 // Multiply returns this vector multiplied by a scalar value.
-func (v vec2[T]) Multiply(n T) vec2[T] {
+func (v Vec2) Multiply(n float64) Vec2 {
 	return NewVec2(
 		v.X*n,
 		v.Y*n,
@@ -55,7 +50,7 @@ func (v vec2[T]) Multiply(n T) vec2[T] {
 }
 
 // Divide returns this vector divided by a scalar value.
-func (v vec2[T]) Divide(n T) vec2[T] {
+func (v Vec2) Divide(n float64) Vec2 {
 	return NewVec2(
 		v.X/n,
 		v.Y/n,
@@ -63,30 +58,28 @@ func (v vec2[T]) Divide(n T) vec2[T] {
 }
 
 // Magnitude returns the length of this vector.
-func (v vec2[T]) Magnitude() float64 {
-	x := float64(v.X)
-	y := float64(v.Y)
-	return math.Sqrt(x*x + y*y)
+func (v Vec2) Magnitude() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
 // Normalised returns the vector in the same direction as this vector with a length of 1.
 //
 // Since a 0-length array has no direction, if |v| = 0 then this function will return an error.
-func (v vec2[T]) Normalised() (vec2[float64], error) {
+func (v Vec2) Normalised() (Vec2, error) {
 	magnitude := v.Magnitude()
 
 	if magnitude == 0 {
-		return vec2[float64]{}, errors.New("tried to normalise a 0-length vector")
+		return Vec2{}, errors.New("tried to normalise a 0-length vector")
 	}
 
 	return NewVec2(
-		float64(v.X)/magnitude,
-		float64(v.Y)/magnitude,
+		v.X/magnitude,
+		v.Y/magnitude,
 	), nil
 }
 
 // Angle computes the angle between v1 and v2, in radians.
-func (v1 vec2[T]) Angle(v2 vec2[T]) (float64, error) {
+func (v1 Vec2) Angle(v2 Vec2) (float64, error) {
 	if v1.Magnitude() == 0 {
 		return 0, errors.New("v1 length is 0, cannot compute angle")
 	}
@@ -95,11 +88,10 @@ func (v1 vec2[T]) Angle(v2 vec2[T]) (float64, error) {
 		return 0, errors.New("v2 length is 0, cannot compute angle")
 	}
 
-	return math.Acos(float64(v1.Dot(v2)) / (v1.Magnitude() * v2.Magnitude())), nil
+	return math.Acos(v1.Dot(v2) / (v1.Magnitude() * v2.Magnitude())), nil
 }
 
 // Lerp linearly interpolates between v1 and v2 by factor t.
-// Note that this function will always return a vec2[float64].
 //
 // Mathematically, this computes v1 * (1 - t) + v2 * t.
 //
@@ -110,10 +102,9 @@ func (v1 vec2[T]) Angle(v2 vec2[T]) (float64, error) {
 // No safeguards are in place for vales of t that do not satisfy 0 <= t <= 1.
 // Instead, this will extrapolate beyond v1 or v2. For example, values of t > 1 will be in the direction of
 // (v2 - v1). t = -2 will result in a vector equal to v1 * 3 + v2 * -2.
-func (v1 vec2[T]) Lerp(v2 vec2[T], t float64) vec2[float64] {
+func (v1 Vec2) Lerp(v2 Vec2, t float64) Vec2 {
 	return NewVec2(
-		float64(v1.X)+t*float64(v2.X-v1.X),
-		float64(v1.Y)+t*float64(v2.Y-v1.Y),
+		v1.X+t*(v2.X-v1.X),
+		v1.Y+t*(v2.Y-v1.Y),
 	)
-	// return v1.Multiply((1 - T(t))).Add(v2.Multiply(T(t)))
 }
