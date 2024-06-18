d99d2f7387cb89707cec71a341bb6b796065799f
Michael Ryan <ryan.m1334@gmail.com>
2024-06-18 12:52:29 +0100

    Document

--- pkg/streams/streams.go
+++ pkg/streams/streams.go
@@ -1,5 +1,6 @@
 package streams
 
+// Map applies a function f to all members of xs, then returns a new slice containing the mapped values.
 func Map[T, U any](xs []T, f func(T) U) []U {
 	ys := make([]U, len(xs))
 
@@ -10,6 +11,7 @@ func Map[T, U any](xs []T, f func(T) U) []U {
 	return ys
 }
 
+// Filter applies a predicate to all members of xs, then returns a new slice containing only the members of xs such that predicate(x) = true.
 func Filter[T any](xs []T, predicate func(T) bool) []T {
 	var ys []T
 
@@ -22,6 +24,9 @@ func Filter[T any](xs []T, predicate func(T) bool) []T {
 	return ys
 }
 
+// ToGenerator returns a channel that will produce all members of xs before closing.
+//
+// This function performs the inverse operation of [Collect].
 func ToGenerator[T any](xs []T) <-chan T {
 	generator := make(chan T)
 
@@ -35,6 +40,11 @@ func ToGenerator[T any](xs []T) <-chan T {
 	return generator
 }
 
+// Collect returns a slice that contains all values produced by xs before closing.
+//
+// This function performs the inverse operation of [ToGenerator].
+//
+// Note that if xs never closes, this function will block indefinitely.
 func Collect[T any](xs <-chan T) []T {
 	var collected []T
 
